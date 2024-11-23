4e61ccd7d1a4935d45f55002d06dec1869d10aab
Taimander <taimander01@gmail.com>
2024-11-22 23:00:10 -0600

    implement keyboard and key recognition

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ from hand_recognition import HandRecognition
 from voice_recognition import VoiceRecognition
 from threading import Thread
 from python_event_bus import EventBus
+import buttons
+
+keyboard_img = cv2.imread('kbd.png', cv2.IMREAD_UNCHANGED)
+
+# Split overlay into BGR and Alpha channels
+overlay_bgr = keyboard_img[:, :, :3]
+overlay_alpha = keyboard_img[:, :, 3] / 255.0  # Normalize alpha to 0-1
+
 
 # Open a video capture object (0 for the default camera)
 cap = cv2.VideoCapture(0)
@@ -19,19 +27,39 @@ last_pos = None
 def on_click():
     global last_pos
     if last_pos:
-        print(f'Click en la posicion {last_pos}')
+        # print(f'Click en la posicion {last_pos}')
+        x, y = last_pos
+        key = buttons.get_button(x, y)
+        if key:
+            print(f'Click en la tecla {key}')
 
 while cap.isOpened():
     ret, frame = cap.read()
     
     if not ret:
         continue
+
+    # If frame is not 640x480, resize it
+    if frame.shape[0] != 480 or frame.shape[1] != 640:
+        frame = cv2.resize(frame, (640, 480))
+    
+    # Flip frame
+    frame = cv2.flip(frame, 1)
     
     landmark = hand_req.process_hand(frame)
     if landmark:
         last_pos = landmark
         x, y = landmark
         cv2.circle(frame, (x, y), 5, (0, 255, 0), -1)
+    
+    # Display the keyboard image
+
+    # Blend the overlay with the webcam frame
+    for c in range(3):  # Apply alpha blending to each channel
+        frame[:, :, c] = (
+            overlay_alpha * overlay_bgr[:, :, c] +
+            (1 - overlay_alpha) * frame[:, :, c]
+        )
     
     # Display the frame with hand landmarks
     cv2.imshow('Hand Recognition', frame)
